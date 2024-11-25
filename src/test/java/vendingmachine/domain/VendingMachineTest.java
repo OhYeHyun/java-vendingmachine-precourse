fdea7ff1dc8ab483f2e1959556bb33c19d3b434e
OhYeHyun <ohyhohyl@naver.com>
2024-11-25 22:34:44 +0900

    Test(VendingMachine): 잔돈 계산하는 기능 테스트

---
+++ src/test/java/vendingmachine/domain/VendingMachineTest.java
@@ -0,0 +1,24 @@
+package vendingmachine.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class VendingMachineTest {
+
+    @Test
+    @DisplayName("잔돈 계산이 올바르게 나오는지 확인")
+    void 잔돈_테스트() {
+        CoinHistory coinHistory = CoinHistory.getInstance();
+        coinHistory.setCoin(Coin.COIN_100, 4);
+        coinHistory.setCoin(Coin.COIN_50, 1);
+
+        VendingMachine machine = new VendingMachine(CoinHistory.getInstance());
+        Map<Coin, Integer> result = machine.receiveCoin(500);
+
+        assertThat(result.get(Coin.COIN_100)).isEqualTo(4);
+        assertThat(result.get(Coin.COIN_50)).isEqualTo(1);
+    }
+}
