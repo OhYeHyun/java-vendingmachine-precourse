bdf9eba394debecefb5ac2f473bc240ac361556a
OhYeHyun <ohyhohyl@naver.com>
2024-11-27 23:51:18 +0900

    feat(InputView): 입력받는 기능

---
+++ src/main/java/vendingmachine/view/VendingMachineInputView.java
@@ -0,0 +1,56 @@
+package vendingmachine.view;
+
+import java.util.Scanner;
+import vendingmachine.validator.AmountFormatValidator;
+import vendingmachine.validator.ProductFormatValidator;
+
+public class VendingMachineInputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static String getInput() {
+        return scanner.nextLine();
+    }
+
+    public static String getAmount(boolean isFirst) {
+        while (true) {
+            VendingMachineOutputView.instructionAmount(isFirst);
+            String amount = getInput();
+            try {
+                AmountFormatValidator.validate(amount);
+                return amount;
+            } catch (IllegalArgumentException e) {
+                VendingMachineOutputView.printWithLineSpace(e.getMessage());
+            }
+        }
+    }
+
+    public static String getInputAmount() {
+        while (true) {
+            VendingMachineOutputView.instructionInputAmount();
+            String amount = getInput();
+            try {
+                AmountFormatValidator.validate(amount);
+                return amount;
+            } catch (IllegalArgumentException e) {
+                VendingMachineOutputView.printWithLineSpace(e.getMessage());
+            }
+        }
+    }
+
+    public static String getProducts() {
+        while (true) {
+            VendingMachineOutputView.instructionProduct();
+            String products = getInput();
+            try {
+                ProductFormatValidator.validate(products);
+                return products;
+            } catch (IllegalArgumentException e) {
+                VendingMachineOutputView.printWithLineSpace(e.getMessage());
+            }
+        }
+    }
+
+    public static String getPurchaseProduct() {
+        return getInput();
+    }
+}
