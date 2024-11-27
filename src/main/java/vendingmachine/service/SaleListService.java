a189bad18449076f790c99c01d2c15fcf32f7341
OhYeHyun <ohyhohyl@naver.com>
2024-11-28 00:47:51 +0900

    fix(SaleListService): 추가할 상품을 모두 유효성 검사 후 saleList 에 추가하도록 변경

--- src/main/java/vendingmachine/service/SaleListService.java
+++ src/main/java/vendingmachine/service/SaleListService.java
@@ -1,6 +1,9 @@
 package vendingmachine.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import vendingmachine.domain.Product;
 import vendingmachine.domain.SaleList;
 import vendingmachine.domain.SaleProduct;
@@ -8,17 +11,23 @@ import vendingmachine.errorMessage.SaleListErrorMessage;
 
 public class SaleListService {
     private final SaleList saleList;
+    private final List<SaleProduct> productToAdd = new ArrayList<>();
 
     public SaleListService() {
         this.saleList = SaleList.getInstance();
     }
 
-    public void addProduct(String productName, int price, int quantity) {
+    public void validateProduct(String productName, int price, int quantity) {
         validateProductName(productName);
         validatePrice(price);
         validateQuantity(quantity);
 
-        saleList.addSaleProduct(new SaleProduct(new Product(productName, price), quantity));
+        productToAdd.add(new SaleProduct(new Product(productName, price), quantity));
+    }
+
+    public void addProduct() {
+        productToAdd.forEach(saleList::addSaleProduct);
+        productToAdd.clear();
     }
 
     public void purchaseProduct(String productName) {
@@ -26,29 +35,33 @@ public class SaleListService {
     }
 
     public boolean isRemainQuantity() {
-        int remainQuantity = saleList.getSaleList().stream().mapToInt(product -> product.getQuantity()).sum();
+        int remainQuantity = saleList.getSaleList().stream().mapToInt(SaleProduct::getQuantity).sum();
         return remainQuantity > 0;
     }
 
     private void validatePrice(int price) {
         if (price < 100) {
+            productToAdd.clear();
             throw new IllegalArgumentException(SaleListErrorMessage.BELOW_MINIMUM_PRICE.getMessage());
         }
 
         if (price % 10 != 0) {
+            productToAdd.clear();
             throw new IllegalArgumentException(SaleListErrorMessage.MUST_BE_10_UNITS_PRICE.getMessage());
         }
     }
 
     private void validateQuantity(int quantity) {
         if (quantity < 1) {
+            productToAdd.clear();
             throw new IllegalArgumentException(SaleListErrorMessage.MUST_BE_EXIST_QUANTITY.getMessage());
         }
     }
 
     private void validateProductName(String productName) {
         try {
-            saleList.findSaleProduct(productName);
+            productToAdd.stream().filter(product -> Objects.equals(product.getProduct().getName(), productName)).findFirst().get();
+            productToAdd.clear();
             throw new IllegalArgumentException(SaleListErrorMessage.ALREADY_EXIST_PRODUCT.getMessage());
         } catch (NoSuchElementException ignored) {
         }
